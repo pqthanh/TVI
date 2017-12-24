79cb36a1444e4704b04c284a0a89064341c76056
PqThanh <thanh.phan@vinasource.com>
2017-12-24 21:14:45 +0700

    improve scan qr code

--- TVI/ViewController/ScanViewController.swift
+++ TVI/ViewController/ScanViewController.swift
@@ -11,21 +11,39 @@ import SwiftQRCode
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var viewPayment: UIView!
     let scanner = QRCode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.viewPayment.isHidden = true
         scanner.prepareScan(view) { (stringValue) -> () in
             if stringValue != "" {
                 print(stringValue)
                 self.scanner.stopScan()
                 self.scanner.removeAllLayers()
+                self.viewPayment.isHidden = false
             }
         }
         scanner.scanFrame = view.bounds
     }
 
+    @IBAction func paymentAction() {
+        self.view.endEditing(true)
+        self.viewPayment.isHidden = true
+        scanner.startScan()
+        self.scanner.removeAllLayers()
+        scanner.prepareScan(view) { (stringValue) -> () in
+            if stringValue != "" {
+                print(stringValue)
+                self.scanner.stopScan()
+                self.scanner.removeAllLayers()
+                self.viewPayment.isHidden = false
+            }
+        }
+    }
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         // start scan
