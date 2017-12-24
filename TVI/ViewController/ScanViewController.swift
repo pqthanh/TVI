5c4146f559d0d6ed82307fb16d815dca8df5c66c
pqthanh <thanh.phan@vinasource.com>
2017-12-24 18:18:09 +0700

    scan qrcode

--- TVI/ViewController/ScanViewController.swift
+++ TVI/ViewController/ScanViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import SwiftQRCode
 
 class SecondViewController: UIViewController {
 
+    let scanner = QRCode()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        scanner.prepareScan(view) { (stringValue) -> () in
+            if stringValue != "" {
+                print(stringValue)
+                self.scanner.stopScan()
+                self.scanner.removeAllLayers()
+            }
+        }
+        scanner.scanFrame = view.bounds
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // start scan
+        scanner.startScan()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
